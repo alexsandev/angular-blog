47c49694c1c1720997e54b6ffa16c5f457aa5e9a
alexsandev <alex.rsjr10@gmail.com>
2023-10-11 21:43:49 -0300

    created home and services pages, adjuste global CSS and page route module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuBarComponent } from './shared/menu-bar/menu-bar.component';
+import { MenuBarOptionComponent } from './shared/menu-bar-option/menu-bar-option.component';
+import { SectionHeaderComponent } from './shared/section-header/section-header.component';
+import { InfoCardComponent } from './shared/info-card/info-card.component';
+import { ServicesComponent } from './pages/services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InfoCardComponent,
+    MenuBarComponent,
+    MenuBarOptionComponent,
+    SectionHeaderComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
